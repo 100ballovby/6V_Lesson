c888ceabd5bcb7e8edc47606d6c22141c82e80ee
demidraksin <greatraksin@icloud.com>
2021-01-19 09:44:24 +0300

    Initial commit

---
+++ 1201/base_print.py
@@ -0,0 +1,11 @@
+print('Hello, world!')  # функция, которая выводит текст
+
+name = 'Demid'  # переменной name принадлежит значение 'Demid'
+print(f'Hello, {name}!')
+# f - позволяет вставить значение переменной в текст
+# аналогично блоку "объединить" в scratch
+
+friend = input('What is your friend name? ').capitalize()  # capitalize - делает первую букву большой
+# input() - позволяет ввести что-то с клавиатуры
+# аналог блока "спросить и ждать" в scratch
+print(f'Hello, {friend}!')
