e6163fed71ab115f6c7430804f3a6424d7c0f420
demidraksin <greatraksin@icloud.com>
2021-01-26 09:53:49 +0300

    Adding difficult conditions theory

---
+++ 2601/bool_condition.py
@@ -0,0 +1,17 @@
+'''
+a = True  # 1
+b = False  # 0
+
+print(a > b)  # True
+'''
+
+ex1 = True
+ex2 = True
+ex3 = True
+ex4 = True
+mid = 4.5
+
+if (ex1 and ex2 and ex3 and ex4) and (mid > 4):
+    print('OK')
+else:
+    print('Ne ok')
