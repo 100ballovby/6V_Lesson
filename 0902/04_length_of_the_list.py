c979e7aa851487820abeb4cf8df3300e7a989b09
demidraksin <greatraksin@icloud.com>
2021-02-09 10:03:01 +0300

    Adding lists length measuring methods🧠

---
+++ 0902/04_length_of_the_list.py
@@ -0,0 +1,8 @@
+motorcycles = ['honda', 'm1nsk', 'bmw', 'yamaha', 'cf motors', 'ducati']
+cars = ['toyota', 'skoda', 'mercedes', 'seat', 'audi']
+
+'''
+Чтобы измерить длину списка, используем функцию len()
+'''
+print('Длина списка motorcycles =', len(motorcycles))
+print('Длина списка cars =', len(cars))
