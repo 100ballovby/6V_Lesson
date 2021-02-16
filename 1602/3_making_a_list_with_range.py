6f7b6abc60e3c175954caf95f9072eb717edc884
demidraksin <greatraksin@icloud.com>
2021-02-16 10:09:37 +0300

    Adding practical🌚

---
+++ 1602/3_making_a_list_with_range.py
@@ -0,0 +1,10 @@
+for i in range(1, 11):
+    print('12 *', i, '=', 12 * i)
+    
+#########################
+
+squares = []
+# список, в котором я буду хранить квадраты чисел
+for value in range(1, 10):
+    squares.append(value ** 2)
+print(squares)
