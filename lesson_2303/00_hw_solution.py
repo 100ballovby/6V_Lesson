8bd9bae33b570039d84711bccc38c6e565868114
demidraksin <greatraksin@icloud.com>
2021-03-23 10:26:18 +0300

    ❄️🐋️Adding strings theory🐋❄️

---
+++ lesson_2303/00_hw_solution.py
@@ -0,0 +1,10 @@
+countries = {
+    'USA': 'Washington DC',
+    'France': 'Paris',
+    'Poland': 'Warsaw',
+    'Germany': 'Berlin',
+    'Belarus': 'Minsk',
+}
+
+for country, city in countries.items():
+    print(f'{city} is the capital of {country}!')
